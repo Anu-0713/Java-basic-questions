5a30220016a5f33cde61a7700a76f0db568b2cc4
YNKAnnapuna <136591876+Anu-0713@users.noreply.github.com>
2025-03-12 15:30:46 +0530

    Right side Binary Triangle

--- Patterns/Pattern11.java
+++ Patterns/Pattern11.java
@@ -1,6 +1,5 @@
 /*Problem statement
 Aryan and his friends are very fond of the pattern. For a given integer ‘N’, they want to make the N-Binary Number Triangle.
-
 You are required to print the pattern as shown in the examples below.
 
 Example:
