8ad38a90b86c3c33f99f33ef32f45cd83eddff1f
YNKAnnapuna <136591876+Anu-0713@users.noreply.github.com>
2025-07-19 11:28:41 +0530

    Reverse Star Triangle

--- Patterns/Pattern 8.java
+++ Patterns/Pattern 8.java
@@ -14,12 +14,12 @@ public class Solution {
     public static void nStarTriangle(int n) {
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<i;j++)
+            for(int j=0;j<i;j++) //Since its reverse the gap should be equal to the number in the row 
             {
                 System.out.print(" ");
 
             }
-            for(int j=0;j<2*n-(2*i+1);j++)
+            for(int j=0;j<2*n-(2*i+1);j++)//Example n=5 then j should be less than 2x5-(2x1+1)=10-3=7
             {
                 System.out.print("*");
             }
